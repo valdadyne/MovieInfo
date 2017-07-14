eeddba77e03725754fe992fca3bd4dab53dddbc3
Rodgers M Gitau <rodgers.gitau@gmail.com>
2017-07-14 13:42:41 +0300

    Delete store.js

--- src/DataStore/store.js
+++
@@ -1,9 +0,0 @@
-// Declaring the module dependency
-var MongoPortable = require("mongo-portable");
-
-// Instantiates a new ddbb object by passing a ddbb name
-var db = new MongoPortable("TEST");
-
-// Creates a new collection named "users"
-//      (if it's already created, it will just return it instead)
-var users = db.collection("users");
